bbad404cf3719b59ba2f30dd8487c406e8dbeba8
arifkhan101 <arifkhan230122@gmail.com>
2023-12-30 18:36:28 +0600

    first commit

---
+++ client/src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2 className="text-3xl font-bold">404 not found</h2>
+        </div>
+    );
+};
+
+export default ErrorPage;
